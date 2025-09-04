effcd7409d5282950142983e8ea43c7ec45513bb
云雨 <lvynyu2@gmail.com>
2025-09-04 17:57:42 +0800

    更新

--- tool_web/VFontX/main.js
+++ tool_web/VFontX/main.js
@@ -49,7 +49,7 @@ getElementMix('data-addlocalfont-btn').addEventListener('click',async ()=>{
 });
 */
 
-class FontManager1 {
+class FontManager {
   constructor() {
     this.dirHandle = null;
     this.fonts = [];
@@ -262,7 +262,7 @@ class FontManager1 {
 /**
      * 字体管理器 - 支持持久化权限 + 子目录分类
      */
-class FontManager {
+class FontManager1 {
   constructor() {
     this.dirHandle = null;
     this.fontMap = new Map(); // folderPath → fonts[]
